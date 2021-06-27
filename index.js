e857aeff0fa335c3513a5ea36338edc339f2372d
PRERNA <Choudharyprerna493@gmail.com>
2021-06-27 21:37:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function myfun() {
+    var m = parseFloat(document.getElementById('weight').value);
+    var h = parseFloat(document.getElementById('height').value);
+    var bmi = m / (h * h);
+    bmi = Math.round(bmi);
+    document.getElementById('bmi').innerHTML = bmi;
+    console.log(Math.round(bmi));
+    message(bmi);
+}
+function message(bmi) {
+  
+    if (bmi < 18) {
+        document.getElementById('msg1').style.display="block";
+    }
+    else if ((bmi >= 18)||(bmi<25)) {
+        document.getElementById('msg2').style.display="block";
+    }
+    else if (bmi >=25) {
+        document.getElementById('msg3').style.display="block";
+    }
+
+}
